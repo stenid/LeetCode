08bf2993f4355b34f6e492a0b7ad1d1c25191553
stenid <stenid8@gmail.com>
2023-01-17 01:25:31 +0700

    TwoSum problem

---
+++ src/twoSum/BetterSolution.java
@@ -0,0 +1,35 @@
+package twoSum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BetterSolution {
+    public int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> complements = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            Integer complementIndex = complements.get(nums[i]);
+            if(complementIndex != null) {
+                return new int[] {i, complementIndex};
+            }
+            complements.put(target - nums[i], i);
+
+        }
+        return nums;
+    }
+
+
+    /*
+    {5, 2, 4}
+    6
+
+    HashMap
+    K -> V
+    on index 0 we got 5, and we know that we need to add a 1 to get the target(6)
+    => We are assigning to key 1 and to value 0 (index in array).
+    1 -> 0
+    4 -> 1
+    {2, 1}
+
+     */
+}

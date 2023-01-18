1cdc6b1c61b28c938f20ed75811f20ef46bdfd31
stenid <stenid8@gmail.com>
2023-01-18 18:50:47 +0700

    Running summary of 1d Array 1480

--- src/runningSumOf1dArray_1480/Solution.java
+++ src/runningSumOf1dArray_1480/Solution.java
@@ -32,3 +32,30 @@ Input: nums = [1,1,1,1,1]
 Output: [1,2,3,4,5]
 Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
  */
+
+/**
+ * code from Main class:
+ *
+ * import runningSumOf1dArray_1480.SecondSolution;
+ * import runningSumOf1dArray_1480.Solution;
+ *
+ * import java.util.Arrays;
+ *
+ * public class Main {
+ *     public static void main(String[] args) {
+ *         var solution = new Solution();
+ *         int[] input = {1, 1, 1, 1, 1};
+ *         int[] result = solution.runningSum(input);
+ *         System.out.println("Input: " + Arrays.toString(input));
+ *         System.out.println("Result: " + Arrays.toString(result));
+ *
+ *         var secondSolution = new SecondSolution();
+ *         int[] secondResult = solution.runningSum(input);
+ *         System.out.println("----------------------------------");
+ *         System.out.println("Second solution");
+ *         System.out.println("Input: " + Arrays.toString(input));
+ *         System.out.println("Result: " + Arrays.toString(result));
+ *
+ *     }
+ * }
+ */

97f9fa558e142a23aaf59e872bd7b64deb5acdd2
stenid <stenid8@gmail.com>
2023-01-24 17:55:57 +0700

    1342. Number of steps to reduce a number to zero.

---
+++ src/reduceNumberToZero/Main.java
@@ -0,0 +1,10 @@
+package reduceNumberToZero;
+
+public class Main {
+    public static void main(String[] args) {
+        var solution = new Solution();
+        var secondSolution = new SecondSolution();
+        System.out.println(solution.numberOfSteps(123));
+        System.out.println(secondSolution.numberOfSteps(123));
+    }
+}
---
+++ src/reduceNumberToZero/SecondSolution.java
@@ -0,0 +1,29 @@
+package reduceNumberToZero;
+
+public class SecondSolution {
+    public int numberOfSteps(int num) {
+        int steps = 0;
+        while (num > 0) {
+            if ((num & 1) == 0)     // num: xxxxxxx0 & bitmask: 00000001
+                num >>= 1;          // same as num = num >> 1;
+            else
+                num--;
+
+            steps++;
+        }
+        return steps;
+    }
+}
+/**
+ * That solution solves problem through bitwise operations.
+ *
+ *      In binary representation of integer if last digit is 0 (00101110) means that integer is even.
+ *      Otherwise, if last digit is 1 (00010111) - that integer is odd.
+ *
+ *      >> moves all digits to the right in the binary representation of integer
+ *      and add 0 at the beginning.
+ *      When you move all digits to the right on 1 position it's equally to
+ *      dividing by 2.
+ *
+ *      When you subtract 1 from integer, in binary representation last 1 digit become 0.
+ */

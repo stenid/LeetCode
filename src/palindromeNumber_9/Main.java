ad2411b594d1406e2a2f920e56f43c9178e5e085
stenid <stenid8@gmail.com>
2023-01-27 19:38:28 +0700

    9. Palindrome number.

---
+++ src/palindromeNumber_9/Main.java
@@ -0,0 +1,10 @@
+package palindromeNumber_9;
+
+public class Main {
+    public static void main(String[] args) {
+        var solution = new Solution();
+        System.out.println("First case, input is 121: " + solution.isPalindrome(121));
+        System.out.println("Second case, input is -121: " + solution.isPalindrome(-121));
+        System.out.println("Third case, input is 10: " + solution.isPalindrome(10));
+    }
+}
---
+++ src/palindromeNumber_9/Solution.java
@@ -0,0 +1,51 @@
+package palindromeNumber_9;
+
+public class Solution {
+    public boolean isPalindrome(int x) {
+        String value = Integer.toString(x);
+        StringBuilder reversedValue = new StringBuilder();
+
+        for (int i = (value.length() - 1); i >= 0; i--) {
+            reversedValue.append(value.charAt(i));             /* If we use a concatenation here, every iteration
+                                                               JVM will create a new string object because
+                                                               Strings are immutable in Java. Otherwise,
+                                                               StringBuilder(with append() method) is mutable.
+                                                               So we get rid of unnecessary memory consumption.
+                                                                */
+        }
+        return value.equals(reversedValue.toString());
+    }
+
+}
+
+        /*
+        Given an integer x, return true if x is a
+palindrome
+, and false otherwise.
+
+
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore, it is not a palindrome.
+
+
+Constraints:
+
+-231 <= x <= 231 - 1
+
+
+Follow up: Could you solve it without converting the integer to a string?
+         */

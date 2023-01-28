deb5127a2e9d56c22946c78d849ba22c3e68ea03
stenid <stenid8@gmail.com>
2023-01-28 16:10:03 +0700

    13. Roman to integer.

---
+++ src/romanToInteger_13/Main.java
@@ -0,0 +1,10 @@
+package romanToInteger_13;
+
+public class Main {
+    public static void main(String[] args) {
+        var solution = new Solution();
+        System.out.println("Case 1, III : " + solution.romanToInt("III"));
+        System.out.println("Case 2, LVIII : " + solution.romanToInt("LVIII"));
+        System.out.println("Case 3, MCMXCIV : " + solution.romanToInt("MCMXCIV"));
+    }
+}
